830b81a5a27afa72bdc98955056039578724933e
Corbin Phipps <cmp99@uw.edu>
2020-10-17 16:39:37 -0700

    fixed some backend stuff

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,11 @@ app.listen(PORT);
 
 
 app.get('/domestic', (req, res) => {
-    const results = fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=domestic%20violence%20service&inputtype=textquery&fields=formatted_address,name,opening_hours&locationbias=ipbias&key=AIzaSyC3yYAr2OuKBAlkcJMYOPfiGuDVcRVmLPo')
+    const results = fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=domestic%20violence%20service&inputtype=textquery&fields=formatted_address,name,opening_hours&locationbias=ipbias&key=AIzaSyBTy4elZc3vmAeYUOKVKbORdUAzNba3pY4')
         .then(response => response.json())
         .then(jsonResponse => {
             if (jsonResponse.candidates) {
+                console.log(jsonResponse);
                 return jsonResponse.candidates.map(candidate => {
                     return {
                         address: candidate.formatted_address,
@@ -22,13 +23,16 @@ app.get('/domestic', (req, res) => {
                 })
             }
         })
-    console.log(results);
     res.send(results);
 });
+
 
 fetch('http://localhost:8000/domestic')
     .then(response => {
         if (response.ok) {
             return response.json();
         }
+    })
+    .then(response => {
+        console.log(response);
     })
