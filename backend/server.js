adcb58932a911ad1e1c0107d5ffbf43d26c7064f
Corbin Phipps <cmp99@uw.edu>
2020-10-17 16:06:00 -0700

    did some backend stuff

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fetch = require("node-fetch");
+const app = express();
+
+// http://localhost:8000
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
+
+
+app.get('/domestic', (req, res) => {
+    const results = fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=domestic%20violence%20service&inputtype=textquery&fields=formatted_address,name,opening_hours&locationbias=ipbias&key=AIzaSyC3yYAr2OuKBAlkcJMYOPfiGuDVcRVmLPo')
+        .then(response => response.json())
+        .then(jsonResponse => {
+            if (jsonResponse.candidates) {
+                return jsonResponse.candidates.map(candidate => {
+                    return {
+                        address: candidate.formatted_address,
+                        name: candidate.name,
+                        hours: candidate.opening_hours
+                    }
+                })
+            }
+        })
+    console.log(results);
+    res.send(results);
+});
+
+fetch('http://localhost:8000/domestic')
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+    })
