1b3aabb3167ed87e7a26da54d46437b1740ad0d6
Corbin Phipps <cmp99@uw.edu>
2020-10-17 17:35:30 -0700

    FINALLY GOT SOMETHING GOING

--- backend/server.js
+++ backend/server.js
@@ -8,24 +8,13 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT);
 
 
-app.get('/domestic', (req, res) => {
-    const results = fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=domestic%20violence%20service&inputtype=textquery&fields=formatted_address,name,opening_hours&locationbias=ipbias&key=AIzaSyBTy4elZc3vmAeYUOKVKbORdUAzNba3pY4')
-        .then(response => response.json())
-        .then(jsonResponse => {
-            if (jsonResponse.candidates) {
-                console.log(jsonResponse);
-                return jsonResponse.candidates.map(candidate => {
-                    return {
-                        address: candidate.formatted_address,
-                        name: candidate.name,
-                        hours: candidate.opening_hours
-                    }
-                })
-            }
-        })
-    res.send(results);
-});
+app.get('/domestic', async(req, res) => {
+    const response = await fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=domestic+violence&key=AIzaSyBTy4elZc3vmAeYUOKVKbORdUAzNba3pY4')
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
 
+    res.send(jsonResponse);
+})
 
 fetch('http://localhost:8000/domestic')
     .then(response => {
